ceed55204957ecbf0dbbd3f2130d01813e18ec40
Gustavo <gustavo.lages@irriga.net>
2022-05-22 03:17:15 -0300

    model changes

--- controllers/words/wordsController.js
+++ controllers/words/wordsController.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
-const { Word } = require('../../models')
+const { word } = require('../../models')
 const { Op } = require('sequelize')
 
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const words = await Word.findAll()
+    const words = await word.findAll()
 
   return res.status(200).json(words)
 })
