d1ba242f55c732ea4ebaf6d97391d5e469b86583
Gustavo Lages <gustavo.lages@irriga.net>
2022-12-07 13:38:22 -0300

    Correção de dep

--- config/config.js
+++ config/config.js
@@ -1,24 +1,28 @@
 require('dotenv').config(); // this is important!
+import mysql2 from 'mysql2'
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectModule": mysql2,
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectModule": mysql2,
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectModule": mysql2,
   }
 }
